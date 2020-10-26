43409a3f3f94154602d774b9b10219dbf7464ea1
christianJR98 <cristian-1823@live.com.mx>
2020-10-25 18:51:52 -0600

    Updates

---
+++ precache-manifest.d7f9d38feb435748bc3afd07bf681a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ce754a5117603377deea121c58058a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "44a2bed49fb90f64cf11",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c39e5de1106e91b6c24a",
+    "url": "/portfolio/static/css/main.d200ba32.chunk.css"
+  },
+  {
+    "revision": "44a2bed49fb90f64cf11",
+    "url": "/portfolio/static/js/2.887565d9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.887565d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39e5de1106e91b6c24a",
+    "url": "/portfolio/static/js/main.09b7f244.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.96e38e7aeb531cc956d2ca4f82da24df.js"
+  "/portfolio/precache-manifest.d7f9d38feb435748bc3afd07bf681a17.js"
 );
 
 self.addEventListener('message', (event) => {
