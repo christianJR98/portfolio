ba058e4b2682c10d668b6af6fa174a534a1d28ac
Christian <cristian-1823@live.com.mx>
2023-10-01 21:56:09 -0600

    chore: format files

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './footer.styles.scss';
+import "./footer.styles.scss";
 
-const Footer = () =>{
-    return (
-        <footer className="footer text-center d-flex justify-content-center align-items-center">
-            <p className="text-white footer__text">{new Date().getFullYear()}</p>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="footer text-center d-flex justify-content-center align-items-center">
+      <p className="text-white footer__text">{new Date().getFullYear()}</p>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -1,71 +1,74 @@
 // Show inputs error message
-export const showError = (input, message) =>{
+export const showError = (input, message) => {
+  input.classList.add("is-invalid");
 
-    input.classList.add('is-invalid')
-
-    // Return de divs
-    const formGroup = input.parentElement;
-    formGroup.className = 'form-group error';
-    // El query selector toma una clase, id, etiqueta 
-    const small = formGroup.querySelector('small');
-    small.innerText = message;
-}
+  // Return de divs
+  const formGroup = input.parentElement;
+  formGroup.className = "form-group error";
+  // El query selector toma una clase, id, etiqueta
+  const small = formGroup.querySelector("small");
+  small.innerText = message;
+};
 
 // Show succes outline
-export const showSuccess = (input) =>{
-    input.classList.remove('is-invalid')
+export const showSuccess = (input) => {
+  input.classList.remove("is-invalid");
 
-    const formGroup = input.parentElement;
-    formGroup.className = 'form-group';
-}
+  const formGroup = input.parentElement;
+  formGroup.className = "form-group";
+};
 
 // check email is valid
 export const emailIsValid = (input) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(re.test(input.value.trim())){
-        showSuccess(input);
-        return true
-    }else{
-        showError(input, 'Email is not valid');
-        return false
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+    return true;
+  } else {
+    showError(input, "Email is not valid");
+    return false;
+  }
+};
+
+export const checkRequired = (inputArr) => {
+  let returnValue = true;
+  inputArr.forEach(function (input) {
+    //each input is the tag
+    //trim function eliminate the blank spaces
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+      returnValue = false;
+    } else {
+      showSuccess(input);
     }
-}
-
-export const checkRequired = (inputArr) =>{
-    let returnValue = true
-    inputArr.forEach(function(input){
-        //each input is the tag
-        //trim function eliminate the blank spaces
-       if(input.value.trim() === ''){
-           showError(input,`${getFieldName(input)} is required`);
-           returnValue = false
-       }else{
-           showSuccess(input);
-       }
-    })
-    return returnValue
-}
+  });
+  return returnValue;
+};
 
 // Check input length
-export const checkLength = (input,min,max) =>{
-    if(input.value.length < min){
-        showError(input,`${getFieldName(input)} must be at least ${min} characters`);
-    }
-    else if(input.value.length > max){
-        showError(input,`${getFieldName(input)} must be less than ${max} characters`);
-    }
-    else{
-        showSuccess(input);
-    }
-}
+export const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
 
 const getFieldName = (input) => {
-    //Get the first letter and make  uppercase
-    //and get all the string from the position 1
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
+  //Get the first letter and make  uppercase
+  //and get all the string from the position 1
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
 
 /*
 form.addEventListener('submit',function(e){
